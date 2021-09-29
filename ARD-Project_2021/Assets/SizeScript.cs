d58d14fb0941a733b2a920f1e7769166206e0ca7
imseanred <dts.soren@gmail.com>
2021-09-29 16:43:46 +0200

    *Major Update*
    ## Following updates has occured
    - Fixed Scale script for 3D room
    - Created sub folders for OBJ's and Blender files
    - Created a 3D bed with cloth renders inside blender
    - Exported bed as obj and imported into Unity
    -- General bug fixes.
    
    Co-Authored-By: huniche <91188216+huniche@users.noreply.github.com>

--- ARD-Project_2021/Assets/SizeScript.cs
+++ ARD-Project_2021/Assets/SizeScript.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class SizeScript : MonoBehaviour
 {
     //public int's for controlling the different dimensions of the "room" box.
-    private float x;
-    public float Width;
-    public float Length;
-    public float Height;
+    //private int x;
+    public int Width;
+    public int Length;
+    public int Height;
     public GameObject target;
     // Start is called before the first frame update
     void Start(){
+        //explicit conversion of Width from float value to interger
+        //int x = (int)Width;
     }
 
     // Update is called once per frame
     void Update(){
-        //float x = target.transform.localScale.x * Width;
-        target.transform.localScale = new Vector3(0 + Width, 8, 40);
+
+        //float x = target.transform.localScale.x * Width (using x, to convert to int);
+        target.transform.localScale = new Vector3(0 + Width, 0 + Height, 0 + Length);
     }
 }
