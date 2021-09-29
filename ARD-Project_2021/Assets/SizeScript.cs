5dd9672db2858dc70d7f5cf45a313634f7dba2d6
imseanred <dts.soren@gmail.com>
2021-09-29 12:23:51 +0200

    Testing script code
    ## Plan: Make a script that allows public variables to control the scale of the object in different dimensions.
    ----
    
    Curently testing out different solutions
    
    Co-Authored-By: huniche <91188216+huniche@users.noreply.github.com>

--- ARD-Project_2021/Assets/SizeScript.cs
+++ ARD-Project_2021/Assets/SizeScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SizeScript : MonoBehaviour
 {
+    //public int's for controlling the different dimensions of the "room" box.
+    private float x;
+    public float Width;
+    public float Length;
+    public float Height;
+    public GameObject target;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        //float x = target.transform.localScale.x * Width;
+        target.transform.localScale = new Vector3(0 * Width, 8, 40);
     }
 }
